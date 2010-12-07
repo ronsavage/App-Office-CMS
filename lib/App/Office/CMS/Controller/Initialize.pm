cf4713335cfb37bc485e6e470d341c5f5e5f25ee
Ron Savage <ron@savage.net.au>
2010-12-07 15:27:00 +1100

    Experiment with TinyCME theme: simple

--- lib/App/Office/CMS/Controller/Initialize.pm
+++ lib/App/Office/CMS/Controller/Initialize.pm
@@ -169,9 +169,8 @@ var tab_view = new YAHOO.widget.TabView();
 
 tinyMCE.init
 ({
-	editor_selector: "mce_editor",
-	mode:            "specific_textareas",
-	theme:           "advanced"
+	mode:  "textareas",
+	theme: "simple"
 });
 
 EJS
