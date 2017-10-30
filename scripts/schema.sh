09b9afa28be17cbab929c71b60c3032dc13adddf
ronsavage <ron@savage.net.au>
2017-10-30 16:09:33 +1100

    Re-generate the schema using scripts/schema.sh

--- scripts/schema.sh
+++ scripts/schema.sh
@@ -2,11 +2,11 @@
 
 DBI_DSN=dbi:Pg:dbname=cms
 DBI_USER=cms
-DI_PASS=cms
+DBI_PASS=cms
 export DBI_DSN
 export DBI_USER
 export DBI_PASS
 
-dbi.schema.pl svg > docs/cms.schema.svg
+dbi.schema.pl svg docs/cms.schema.svg
 
 echo Wrote docs/cms.schema.png
