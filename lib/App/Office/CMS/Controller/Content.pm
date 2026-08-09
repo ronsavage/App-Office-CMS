27ae6419d90ec79f4773ad9532f83988377b3f9c
Ron Savage <ron@savage.net.au>
2026-08-09 16:36:30 +1000

    Clean up various PODs

--- lib/App/Office/CMS/Controller/Content.pm
+++ lib/App/Office/CMS/Controller/Content.pm
@@ -487,3 +487,38 @@ sub update
 # -----------------------------------------------
 
 1;
+
+=pod
+
+=head1 NAME
+
+C<App::Office::CMS::Controller::Content> - Manage the Canny, Microlight and Simple CMS
+
+=head1 Machine-Readable Change Log
+
+The file Changes was converted into Changelog.ini by L<Module::Metadata::Changes>.
+
+=head1 Version Numbers
+
+Version numbers < 1.00 represent development versions. From 1.00 up, they are production versions.
+
+=head1 Support
+
+Email the author.
+
+=head1 Author
+
+L<CPAN::MetaCurator> was written by Ron Savage I<E<lt>ron@savage.net.auE<gt>> in 2025.
+
+My homepage: L<https://savage.net.au/>.
+
+=head1 Copyright
+
+Australian copyright (c) 2010, Ron Savage.
+
+	All Programs of mine are 'OSI Certified Open Source Software';
+	you can redistribute them and/or modify them under the terms of
+	The Perl License, a copy of which is available at:
+	http://dev.perl.org/licenses/
+
+=cut
